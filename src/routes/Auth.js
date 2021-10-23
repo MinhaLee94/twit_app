8ff715db3abc6a867eae4be40bedaf8d573110ee
MinhaLee94 <shjune1994@gmail.com>
2021-10-23 13:12:37 -0400

    Build basic login feature

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,20 +1,42 @@
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "@firebase/auth";
+import { authService } from "fbase";
 import React, { useState } from "react";
 
 const Auth = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const [newAccount, setNewAccount] = useState(true);
+
 	const onChange = (event) => {
-		console.log(event.target.name);
+		const { target: { name, value }} = event;
+		if(name === "email") {
+			setEmail(value);
+		} else if (name === "password") {
+			setPassword(value);
+		}
 	};
-	const onSubmit = (event) => {
+	const onSubmit = async (event) => {
 		event.preventDefault();
+		try {
+			if(newAccount) {
+				const data = await createUserWithEmailAndPassword(authService, email, password);
+				console.log(data);
+			} else {
+				const data = await signInWithEmailAndPassword(authService, email, password);
+				console.log(data);
+			}
+			
+		} catch(error) {
+			console.log(error);
+		}
 	};
+
 	return (
 		<div>
 			<form onSubmit={onSubmit}>
 				<input name="email" type="text" placeholder="Email" required value={email} onChange={onChange}/>
 				<input name="password" type="password" placeholder="Password" required value={password} onChange={onChange}/>
-				<input type="submit" value="Log in" />
+				<input type="submit" value={newAccount ? "Create Account" : "Log in"} />
 			</form>
 			<div>
 				<button>Continue with Google</button>
