bd8f07431247ca5f45d3314c767998155c922c24
MinhaLee94 <shjune1994@gmail.com>
2021-10-24 23:51:31 -0400

    Toggle login and interval firebase Auth

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -6,6 +6,7 @@ const Auth = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 	const [newAccount, setNewAccount] = useState(true);
+	const [error, setError] = useState("");
 
 	const onChange = (event) => {
 		const { target: { name, value }} = event;
@@ -27,9 +28,12 @@ const Auth = () => {
 			}
 			
 		} catch(error) {
-			console.log(error);
+			setError(error.message);
 		}
 	};
+	const toggleAccount = () => {
+		setNewAccount((prev) => !prev);
+	}
 
 	return (
 		<div>
@@ -37,7 +41,9 @@ const Auth = () => {
 				<input name="email" type="text" placeholder="Email" required value={email} onChange={onChange}/>
 				<input name="password" type="password" placeholder="Password" required value={password} onChange={onChange}/>
 				<input type="submit" value={newAccount ? "Create Account" : "Log in"} />
+				{error}
 			</form>
+			<span onClick={toggleAccount}>{newAccount ? "Sign in" : "Create Account"}</span>
 			<div>
 				<button>Continue with Google</button>
 				<button>Continue with GitHub</button>
