b81754a82937afd4e9bd940907484a97d362b91a
MinhaLee94 <shjune1994@gmail.com>
2021-11-01 23:50:14 -0400

    Build delete tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,12 +1,21 @@
+import { deleteDoc, doc } from "@firebase/firestore";
+import { dbService } from "fbase";
 import React from "react";
 
 const Tweet = ({ tweetObj, isOwner}) => {
+	const onDeleteClick = async () => {
+		const ok = window.confirm("Are you sure you want to delete this tweet?");
+		if (ok) {
+			const tweetTextRef = doc(dbService, "tweets", `${tweetObj.id}`);
+			await deleteDoc(tweetTextRef);
+		}
+	}
 	return (
 		<div>
 			<h4>{tweetObj.text}</h4>
 			{isOwner && (
 				<>
-					<button>Delete tweet</button>
+					<button onClick={onDeleteClick}>Delete tweet</button>
 					<button>Edit tweet</button>
 				</>
 			)}
