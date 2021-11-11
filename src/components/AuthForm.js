5f0eaa1cc3ec65b0c6a55701ffb55a375e9bef3c
MinhaLee94 <shjune1994@gmail.com>
2021-11-10 23:10:19 -0500

    Build AuthForm styles

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -2,6 +2,51 @@ import React, { useState } from "react";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { authService } from "fbase";
 import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const LoginForm = styled.form`
+	width: 100%;
+	max-width: 320px;
+	display: flex;
+	flex-direction: column;
+`;
+
+const LoginInput = styled.input`
+	max-width: 320px;
+	width: 100%;
+	padding: 10px;
+	border-radius: 30px;
+	background-color: rgba(255, 255, 255, 1);
+	margin-bottom: 10px;
+	font-size: 12px;
+	color: black;
+`;
+
+const SubmitBtn = styled(LoginInput)`
+	text-align: center;
+	background: #04aaff;
+	color: white;
+	margin-top: 10;
+	cursor: pointer;
+`;
+
+const AuthError = styled.span`
+	color: tomato;
+	text-align: center;
+	font-weight: 500;
+	font-size: 12px;
+`;
+
+const AuthSwitch = styled.span`
+	color: #04aaff;
+	cursor: pointer;
+	margin-top: 10px;
+	margin-bottom: 50px;
+	display: block;
+	font-size: 12px;
+	text-decoration: underline;
+`;
+
 
 const AuthForm = () => {
 	const [email, setEmail] = useState("");
@@ -31,13 +76,13 @@ const AuthForm = () => {
 	const toggleAccount = () => setNewAccount((prev) => !prev);
 	return (
 		<>
-			<form onSubmit={onSubmit}>
-				<input name="email" type="text" placeholder="Email" required value={email} onChange={onChange}/>
-				<input name="password" type="password" placeholder="Password" required value={password} onChange={onChange}/>
-				<input type="submit" value={newAccount ? "Create Account" : "Log in"} />
-				{error}
-			</form>
-			<span onClick={toggleAccount}>{newAccount ? "Sign in" : "Create Account"}</span>
+			<LoginForm onSubmit={onSubmit}>
+				<LoginInput name="email" type="text" placeholder="Email" required value={email} onChange={onChange}/>
+				<LoginInput name="password" type="password" placeholder="Password" required value={password} onChange={onChange}/>
+				<SubmitBtn type="submit" value={newAccount ? "Create Account" : "Log in"} />
+				{error && <span>{error}</span>}
+			</LoginForm>
+			<AuthSwitch onClick={toggleAccount}>{newAccount ? "Sign in" : "Create Account"}</AuthSwitch>
 		</>
 	)
 }
